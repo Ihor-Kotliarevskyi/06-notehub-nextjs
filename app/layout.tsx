ed6d1fb95cb6f9ce22223d5c75134bad1f356d9e
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-12-25 21:35:12 +0200

    add folders and files of components add styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,13 +25,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+  return (<html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <TanStackProvider>
+          <Header />
+          <main>{children}</main>          
+          <Footer />
+        </TanStackProvider>
       </body>
-    </html>
-  );
+    </html>);
 }
